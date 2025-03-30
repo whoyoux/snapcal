3cf51f55f59eee440bb6db3fce02a434539ff316
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-03-30 19:54:48 +0200

    added simple dashboard

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
-import { Button } from "./ui/button";
+import Link from "next/link";
+import { Button, buttonVariants } from "./ui/button";
 
 function Header() {
 	return (
 		<header className="max-w-screen-md mx-auto py-5 border-b flex items-center justify-between">
-			<h1 className="text-lg font-semibold">SnapCal</h1>
-			<Button>Sign in</Button>
+			<Link href="/">
+				<h1 className="text-lg font-semibold">SnapCal</h1>
+			</Link>
+			{/* <Button>Sign in</Button> */}
+			<Link
+				href="/dashboard"
+				className={buttonVariants({ variant: "default" })}
+			>
+				Sign in
+			</Link>
 		</header>
 	);
 }
