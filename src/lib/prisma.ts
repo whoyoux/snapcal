0c8d20d6e5da524f4b0edecfa77f592973fdf75f
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-03-30 19:51:15 +0200

    added prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+const prismaClientSingleton = () => {
+	// return new PrismaClient({ log: ['query'] })
+	return new PrismaClient();
+	// return new PrismaClient().$extends(withAccelerate());
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
